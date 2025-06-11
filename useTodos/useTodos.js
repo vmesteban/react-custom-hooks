f18f496db0546d433727e0c13ac67001663e7ec1
VictorManuel EstebanMartinez <vmesteban.wanadoo.es@gmail.com>
2025-06-11 19:30:30 +0200

    useTodos Agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './TodoReducer';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
